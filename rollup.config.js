938dddf566f22a02f03578fc9453f658747d8c79
yuarasino <37145593+yuarasino@users.noreply.github.com>
2024-10-13 02:22:27 +0900

    :wrench: Biomeを導入

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from "rollup"
 import typescript from "@rollup/plugin-typescript"
-
+import { defineConfig } from "rollup"
 
 export default defineConfig({
   input: "src/plugin.ts",
   output: {
     file: "dist/plugin.js",
-    format: "commonjs"
+    format: "commonjs",
   },
-  plugins: [typescript()]
+  plugins: [typescript()],
 })
