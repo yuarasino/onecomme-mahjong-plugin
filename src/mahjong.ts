73f9e988d4fed633b203525895d4ce634a5fb178
yuarasino <37145593+yuarasino@users.noreply.github.com>
2024-10-13 22:44:59 +0900

    :rocket: 漢字形式の変換処理を追加

--- src/mahjong.ts
+++ src/mahjong.ts
@@ -3,14 +3,15 @@ import { execFuncOnTextNode, wrapUrlPattern } from "./dom"
 
 // mpsz形式
 const MPSZ_SUIT_PATTERN = /[mpsｍｐｓ]/g
-const MPSZ_RANK_PATTERN = /[0-9０-９]/g
+const MPSZ_RANK_PATTERN = /[0123456789０１２３４５６７８９]/g
 const MPSZ_HONOR_PATTERN = /[zｚ]/g
-const MPSZ_LETTER_PATTERN = /[1-7１-７]/g
+const MPSZ_LETTER_PATTERN = /[1234567１２３４５６７]/g
 const MPSZ_RED_PATTERN = /(?:r5|赤5|ｒ５|赤５)/g
 // 漢字形式
 const HAN_SUIT_PATTERN = /[萬筒索]/g
-const HAN_RANK_PATTERN = /[一二三四五六七八九〇]/g
+const HAN_RANK_PATTERN = /[〇一二三四五六七八九０-９]/g
 const HAN_LETTER_PATTERN = /[東南西北白發中]/g
+const HAN_RED_PATTERN = /(?:ｒ五|赤五|ｒ５|赤５)/g
 // 記号
 const SYMBOL_BAR_PATTERN = /(?:-|－|ー|‐|－|―)/g
 
@@ -32,26 +33,121 @@ export const replaceMahjongTile = (text: string): string => {
  * @param text 対象テキスト
  * @returns 処理済みテキスト
  */
-export const replaceMpszPattern = (text: string): string => {
+export const replaceMpszSuitedPattern = (text: string): string => {
   const s = MPSZ_SUIT_PATTERN
   const r = MPSZ_RANK_PATTERN
   const a = MPSZ_RED_PATTERN
   const b = SYMBOL_BAR_PATTERN
-  const ra = `(?:${r.source}|${a.source})`
-  const rb = `(?:${ra}|${b.source})`
-  const h = new RegExp(`(${rb}*${ra})(${s.source})`, "g")
+  const ra = new RegExp(`(?:${r.source}|${a.source})`, "g")
+  const rb = new RegExp(`(?:${ra.source}|${b.source})`, "g")
+  const h = new RegExp(
+    `(${rb.source}*${ra.source})(${s.source})(?![a-qs-zａ-ｑｓ-ｚ])`,
+    "g",
+  )
   text = text.replace(h, (_, hand: string, suit: string) => {
-    suit = "mps"["mpsｍｐｓ".indexOf(suit) % 3]
+    suit = getNormalizedSuit(suit)
     hand = hand.replace(a, "0")
     hand = hand.replace(b, "-")
     hand = hand.replace(r, (rank) => {
-      rank = "0123456789"["0123456789０１２３４５６７８９".indexOf(rank) % 10]
+      rank = getNormalizedRank(rank)
       const tile = `${suit}${rank}`
       return createTileTag(tile)
     })
     return hand
   })
   return text
+}
+
+/**
+ * テキスト中のmpsz形式字牌を画像に置き換える
+ * @param text 対象テキスト
+ * @returns 処理済みテキスト
+ */
+export const replaceMpszHonorPattern = (text: string): string => {
+  const s = MPSZ_HONOR_PATTERN
+  const r = MPSZ_LETTER_PATTERN
+  const b = SYMBOL_BAR_PATTERN
+  const rb = new RegExp(`(?:${r.source}|${b.source})`, "g")
+  const h = new RegExp(
+    `(${rb.source}*${r.source})(${s.source})(?![a-qs-zａ-ｑｓ-ｚ])`,
+    "g",
+  )
+  text = text.replace(h, (_, hand: string) => {
+    hand = hand.replace(b, "-")
+    hand = hand.replace(r, (rank) => {
+      rank = getNormalizedRank(rank)
+      const tile = `z${rank}`
+      return createTileTag(tile)
+    })
+    return hand
+  })
+  return text
+}
+
+/**
+ * テキスト中の漢字形式数牌を画像に置き換える
+ * @param text 対象テキスト
+ * @returns 処理済みテキスト
+ */
+export const replaceHanSuitedPattern = (text: string): string => {
+  const s = HAN_SUIT_PATTERN
+  const r = HAN_RANK_PATTERN
+  const a = HAN_RED_PATTERN
+  const b = SYMBOL_BAR_PATTERN
+  const ra = new RegExp(`(?:${r.source}|${a.source})`, "g")
+  const rb = new RegExp(`(?:${ra.source}|${b.source})`, "g")
+  const h = new RegExp(`(${rb.source}*${ra.source})(${s.source})`, "g")
+  text = text.replace(h, (_, hand: string, suit: string) => {
+    suit = getNormalizedSuit(suit)
+    hand = hand.replace(a, "〇")
+    hand = hand.replace(b, "-")
+    hand = hand.replace(r, (rank) => {
+      rank = getNormalizedRank(rank)
+      const tile = `${suit}${rank}`
+      return createTileTag(tile)
+    })
+    return hand
+  })
+  return text
+}
+
+/**
+ * テキスト中の漢字形式字牌を画像に置き換える
+ * @param text 対象テキスト
+ * @returns 処理済みテキスト
+ */
+export const replaceHanHonorPattern = (text: string): string => {
+  const r = HAN_LETTER_PATTERN
+  const b = SYMBOL_BAR_PATTERN
+  const rb = new RegExp(`(?:${r.source}|${b.source})`, "g")
+  const h = new RegExp(`(${rb.source}*${r.source})`, "g")
+  text = text.replace(h, (_, hand: string) => {
+    hand = hand.replace(b, "-")
+    hand = hand.replace(r, (rank) => {
+      rank = getNormalizedRank(rank)
+      const tile = `z${rank}`
+      return createTileTag(tile)
+    })
+    return hand
+  })
+  return text
+}
+
+export const getNormalizedSuit = (suit: string): string => {
+  const targets = ["mps", "ｍｐｓ", "萬筒索"].join("")
+  const index = targets.indexOf(suit)
+  return "mps"[index % 3]
+}
+
+export const getNormalizedRank = (rank: string): string => {
+  const targets = [
+    "0123456789",
+    "０１２３４５６７８９",
+    "〇一二三四五六七八九",
+    "〇東南西北白發中",
+  ].join("")
+  const index = targets.indexOf(rank)
+  return "0123456789"[index % 10]
 }
 
 export const createTileTag = (tile: string): string => {
--- test/mahjong.test.ts
+++ test/mahjong.test.ts
@@ -2,8 +2,11 @@ import { describe, expect, test } from "@jest/globals"
 
 import {
   createTileTag,
+  replaceHanHonorPattern,
+  replaceHanSuitedPattern,
   replaceMahjongTile,
-  replaceMpszPattern,
+  replaceMpszHonorPattern,
+  replaceMpszSuitedPattern,
 } from "../src/mahjong"
 
 describe("replaceMahjongTile", () => {
@@ -14,11 +17,101 @@ describe("replaceMahjongTile", () => {
   })
 })
 
-describe("replaceMpszPattern", () => {
+describe("replaceMpszSuitedPattern", () => {
   test("exec check", () => {
-    const text = "正解は98mです"
-    const expected = `正解は${createTileTag("m9")}${createTileTag("m8")}です`
-    const actual = replaceMpszPattern(text)
+    const text = "正解は12mです"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("m2")}です`
+    const actual = replaceMpszSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("mixed tile", () => {
+    const text = "正解は1２mです"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("m2")}です`
+    const actual = replaceMpszSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("mixed tile 2", () => {
+    const text = "正解は12ｍです"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("m2")}です`
+    const actual = replaceMpszSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("red tile", () => {
+    const text = "正解は1mr5pです"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("p0")}です`
+    const actual = replaceMpszSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("no tile", () => {
+    const text = "今日は100pt稼いだ"
+    const expected = "今日は100pt稼いだ"
+    const actual = replaceMpszSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+})
+
+describe("replaceMpszHonorPattern", () => {
+  test("exec check", () => {
+    const text = "正解は12zです"
+    const expected = `正解は${createTileTag("z1")}${createTileTag("z2")}です`
+    const actual = replaceMpszHonorPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("mixed tile", () => {
+    const text = "正解は1２zです"
+    const expected = `正解は${createTileTag("z1")}${createTileTag("z2")}です`
+    const actual = replaceMpszHonorPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("mixed tile 2", () => {
+    const text = "正解は12ｚです"
+    const expected = `正解は${createTileTag("z1")}${createTileTag("z2")}です`
+    const actual = replaceMpszHonorPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("no tile", () => {
+    const text = "今日は100zt稼いだ"
+    const expected = "今日は100zt稼いだ"
+    const actual = replaceMpszHonorPattern(text)
+    expect(actual).toBe(expected)
+  })
+})
+
+describe("replaceHanSuitedPattern", () => {
+  test("exec check", () => {
+    const text = "正解は１２萬です"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("m2")}です`
+    const actual = replaceHanSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("mixed tile", () => {
+    const text = "正解は１二萬です"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("m2")}です`
+    const actual = replaceHanSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+
+  test("red tile", () => {
+    const text = "正解は１萬赤５筒です"
+    const expected = `正解は${createTileTag("m1")}${createTileTag("p0")}です`
+    const actual = replaceHanSuitedPattern(text)
+    expect(actual).toBe(expected)
+  })
+})
+
+describe("replaceHanHonorPattern", () => {
+  test("exec check", () => {
+    const text = "正解は東南です"
+    const expected = `正解は${createTileTag("z1")}${createTileTag("z2")}です`
+    const actual = replaceHanHonorPattern(text)
     expect(actual).toBe(expected)
   })
 })
