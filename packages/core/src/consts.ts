0fcd1f2afd9b5879ca91330cd3a6526b02f1fac6
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-11-17 23:03:47 +0900

    :wrench: Pnpm:Workspaceを導入

---
+++ packages/core/src/consts.ts
@@ -0,0 +1,8 @@
+export const PLUGIN_NAME = "mahjongpretty"
+export const PLUGIN_NAME_JA = "MahjongPretty"
+export const PLUGIN_UID = `net.yuarasino.${PLUGIN_NAME}`
+export const PLUGIN_VERSION = "1.0.0"
+export const PLUGIN_AUTHOR = "yuarasino"
+export const PLUGIN_AUTHOR_JA = "新篠ゆう"
+export const PLUGIN_WEB_EP = `http://localhost:11180/plugins/${PLUGIN_UID}`
+export const PLUGIN_API_EP = `http://localhost:11180/api/plugins/${PLUGIN_UID}`
---
+++ packages/plugin/src/plugin.ts
@@ -0,0 +1,5 @@
+import * as consts from "@mahjongpretty/core/src/consts"
+
+export default {
+  name: consts.PLUGIN_NAME_JA
+}
