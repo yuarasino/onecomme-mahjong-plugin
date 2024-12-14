7db2563ff001a7f44d55cd58ab5ca227d08ae778
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-12-14 18:20:29 +0900

    🎉 v1.2.0リリース

--- packages/core/src/consts.ts
+++ packages/core/src/consts.ts
@@ -1,7 +1,7 @@
 export const PLUGIN_NAME = "onecomme-mahjong-plugin"
 export const PLUGIN_NAME_JA = "わんコメ麻雀プラグイン"
 export const PLUGIN_UID = `net.yuarasino.${PLUGIN_NAME}`
-export const PLUGIN_VERSION = "1.1.0"
+export const PLUGIN_VERSION = "1.2.0"
 export const PLUGIN_AUTHOR = "yuarasino"
 export const PLUGIN_AUTHOR_JA = "新篠ゆう"
 export const PLUGIN_WEB_EP = `http://localhost:11180/plugins/${PLUGIN_UID}`
